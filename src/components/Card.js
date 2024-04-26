67142d5d30779677de2f8a6a5c3f05fa61c1d3bb
AkinariKunXx <sip4ik89@gmail.com>
2024-04-26 11:36:32 +0300

    Карточки, check

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,3 +1,5 @@
+import check from "../media/check.svg";
+
 export default function Card({ card }) {
     return (
         <div className="w-100pc p-10">
@@ -5,6 +7,28 @@ export default function Card({ card }) {
                 <img className="h-96 object-cover" src={card.src} alt="" />
                 <p className="fw-600 white fs-m3 mt-3">
                     {card.title}
+                </p>
+                <div className="indigo fs-s3 italic after-arrow-right my-4">
+                    Подробнее
+                </div>
+            </a>
+        </div>
+    );
+};
+
+export function CardCatalog({ card }) {
+    const advJsx = card.advantages.map(text => (<p>
+        <img src={check} style={{
+            height: "30px"
+        }} />
+        {text}
+    </p>))
+    return (
+        <div className="w-100pc p-10">
+            <a href="#" className="block no-underline p-5 br-8 hover-bg-indigo-lightest-10 hover-scale-up-1 ease-300 flex justify-center items-centr flex-col">
+                <img className="h-96 object-cover" src={card.src} alt="" />
+                <p className="fw-600 white fs-m3 mt-3">
+                    {advJsx}
                 </p>
                 <div className="indigo fs-s3 italic after-arrow-right my-4">
                     Подробнее
--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -1,7 +1,78 @@
+import { CardCatalog as Card } from "../components/Card";
+
+const cards=[
+    {
+        src: "images/pc5.webp",
+        advantages: [
+            'asdasd',
+            'asdasdasds',
+            'asdasdsd',
+            'asdasdsd',
+            'asdasdsd',
+        ]
+    },
+    {
+        src: "images/pc5.webp",
+        advantages: [
+            'asdasd',
+            'asdasdasds',
+            'asdasdsd',
+            'asdasdsd',
+            'asdasdsd',
+        ]
+    },
+    {
+        src: "images/pc5.webp",
+        advantages: [
+            'asdasd',
+            'asdasdasds',
+            'asdasdsd',
+            'asdasdsd',
+            'asdasdsd',
+        ]
+    },
+    {
+        src: "images/pc5.webp",
+        advantages: [
+            '',
+            'asdasdasds',
+            'asdasdsd',
+            'asdasdsd',
+            'asdasdsd',
+        ]
+    },
+    {
+        src: "images/pc5.webp",
+        advantages: [
+            'asdasd',
+            'asdasdasds',
+            'asdasdsd',
+            'asdasdsd',
+            'asdasdsd',
+        ]
+    },
+    {
+        src: "images/pc5.webp",
+        advantages: [
+            'asdasdas',
+            'asdasdasds',
+            'asdasdsd',
+            'asdasdsd',
+            'asdasdsd',
+        ]
+    },
+];
+
 function Catalog() {
+    let cardsjsx = cards.map((card, key) => <Card card={card} key={key} />);
+    
     return(
         <>
-			Hello!
+            <section className="p-0 md-p-5">
+                <div className="grid desktop:grid-cols-3 tablet:grid-cols-2 grid-cols-1">
+                    {cardsjsx}
+                </div>
+            </section>
         </>
     )
 }
