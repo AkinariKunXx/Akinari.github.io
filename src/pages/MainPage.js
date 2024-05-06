e336325b0b7ee771dfc3dfac4b5d7b63e77638e6
Akinari <sip4ik89@gmail.com>
2024-05-06 18:12:17 +0300

    fff

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -184,15 +184,6 @@ function MainPage() {
             
             <section className="p-10 md-p-l10">
                 <div className="md-w-75pc">
-                    <h2 className="white fs-l3 fw-900 lh-1">
-                        Lorem ipsum dolor sit amet, consectetur
-                    </h2>
-                    <p className="indigo-lightest fw-600 fs-m1 opacity-50 my-5">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor{" "}
-                    </p>
-                </div>
-                <div className="relative w-100pc h-50vh bg-cover bg-b" style={{ backgroundImage: "url(images/pc12.jpg)" }}>
                 </div>
             </section>
 
