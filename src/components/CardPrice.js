88829e1d2a26735eaebf660109be6f19316897fb
AkinariKunXx <sip4ik89@gmail.com>
2024-05-06 13:29:09 +0300

    MainPAge

--- src/components/CardPrice.js
+++ src/components/CardPrice.js
@@ -7,36 +7,16 @@ import {
   Button,
 } from "@material-tailwind/react";
    
-  function CheckIcon() {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={2}
-        stroke="currentColor"
-        className="h-3 w-3"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M4.5 12.75l6 6 9-13.5"
-        />
-      </svg>
-    );
-  }
+  
    
   export default function PricingCard({ card }) {
     let advJsx = card.adv.map((elem, i) => {
       return (<li className="flex items-center gap-4" key={i}>
-        <span className="rounded-full border border-white/20 bg-white/20 p-1">
-          <CheckIcon />
-        </span>
         <Typography className="font-normal">{elem}</Typography>
       </li>)
     })
     return (
-      <Card color="gray" variant="gradient" className="w-full max-w-[20rem] p-8">
+      <Card className="bg-indigo-lightest-10 w-full max-w-[20rem] p-8">
         <CardHeader
           floated={false}
           shadow={false}
@@ -58,22 +38,20 @@ import {
             <img className="w-100pc" src={card.src} />
           </Typography>
         </CardHeader>
-        <CardBody className="p-0">
-          <ul className="flex flex-col gap-4">
+        <CardBody className="flex flex-col justify-between h-100 mt-12 p-0">
+          <ul className="p-0">
             {advJsx}
           </ul>
-        </CardBody>
-        <CardFooter className="mt-12 p-0">
           <Button
             size="lg"
-            color="white"
-            className="hover:scale-[1.02] focus:scale-[1.02] active:scale-100"
+            color="bg-indigo white fs-s3 br-4 f"
+            className="hover:scale-[1.02] focus:scale-[1.02] active:scale-100 bg-indigo white fs-s3 black fw-600 no-underline"
             ripple={false}
             fullWidth={true}
           >
-          Buy Now
-        </Button>
-      </CardFooter>    
+            Buy Now
+          </Button>
+        </CardBody>    
       </Card>
     );
   }
