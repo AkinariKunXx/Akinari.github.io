a88f739deae2bae10c0803e8791d8e2ba33a8652
Akinari <sip4ik89@gmail.com>
2024-05-15 03:58:43 +0300

    full

--- src/pages/Shares.js
+++ src/pages/Shares.js
@@ -4,7 +4,7 @@ function Shares() {
             {/*  hero section */}
 
             <section className="min-h-50vh flex justify-start items-center relative before:content-[''] before:w-full before:h-full before:absolute before:top-0 before:left-0 before:-z-10 before:bg-black/50">
-                <img src="images/pc17.jpg" className="object-center absolute w-full h-full -z-20 top-0" />
+                <img src="images/pc17.jpg" className="object-top object-cover absolute w-full h-full -z-20 top-0" />
                 <div className="mx-5 md-mx-l5">
                     <div className="font-defaultFont text-4xl inline-block br-round bg-indigo-30 white p-3">
                         Акции
